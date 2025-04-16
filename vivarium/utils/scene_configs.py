f3823cbfee6c827fa24cbe9d02642c96f277da51
Clément Moulin-Frier <clement.moulinfrier@gmail.com>
2025-04-16 22:26:09 +0200

    Improve SceneConfiguration

--- vivarium/utils/scene_configs.py
+++ vivarium/utils/scene_configs.py
@@ -1,14 +1,26 @@
 import os 
-import re
 import random
 import logging
-import importlib
 from math import pi
+from dataclasses import dataclass, fields
+from collections.abc import Iterable
 
 from omegaconf import OmegaConf, DictConfig
 from hydra.core.global_hydra import GlobalHydra
 from hydra import initialize, compose
 import hydra
+from typing import Type
+
+import jax.numpy as jnp
+
+from vivarium.environments.state import create_state_cls
+from vivarium.utils.converters import import_class
+
+
+# from vivarium.controllers.simulator_controller import ControllerEntity
+# from vivarium.controllers.panel_controller import ParamEntity
+# from vivarium.controllers.simulator_controller import ControllerEntity
+# from vivarium.controllers.panel_controller import  PanelControllerEntity
 
 
 abs_config_dir_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "../../conf"))
@@ -51,49 +63,136 @@ def load_scene_config(scene_name: str, seed=None) -> DictConfig:
         cfg = hydra.compose(config_name="config", overrides=[f"scene={scene_name}"])
         logging.basicConfig(level=cfg.log_level)
         scene_config = OmegaConf.merge(cfg.default, cfg.scene)
-        for entity_type in scene_config['state_data']['entity_state']['entity_types']:
-            entity_params = scene_config['state_data'][entity_type]['kwargs']
-            n = entity_params['n_exists']
-            if entity_params['position'] == 'None':
-                # Generate random positions if not provided
-                entity_params['position'] = generate_random_positions(n, scene_config['environment']['kwargs']['box_size'], seed)
-            if entity_params['orientation'] == 'None':
-                # Generate random orientations if not provided
-                entity_params['orientation'] = generate_random_orientations(n, seed)
-            scene_config['state_data'][entity_type]['kwargs'] = entity_params
+
         return scene_config
 
-
-def import_class(class_path):
-    if not isinstance(class_path, str):
-        return class_path
-    module_name, class_name = class_path.rsplit('.', 1)
-    module = importlib.import_module(module_name)
-    return getattr(module, class_name)
-
-
-def class_import_path(cls) -> str:
-    """Get the import path of a given class
-
-    :param cls: The class to get the import path for
-    :return: The import path as a string
-    """
-    return f"{cls.__module__}.{cls.__name__}"
+def extend_kwargs(kwargs, n_exists):
+    """Extend kwargs to n_exists items"""
+    for attr, val in kwargs.items():
+        if isinstance(val, Iterable) and '_all_values_' in val:
+            kwargs[attr] = [val['_all_values_']] * n_exists
+    return kwargs
 
 
-def upper_camel_to_snake(name: str) -> str:
-    """Convert an upper camel case string to snake case
-
-    :param name: The string in upper camel case
-    :return: The string in snake case
-    """
-    return re.sub(r'(?<!^)(?=[A-Z])', '_', name).lower()
+@dataclass
+class ConstructorConfiguration:
+    cls: Type
+    kwargs: dict
 
 
-def snake_to_upper_camel(name: str) -> str:
-    """Convert a snake case string to upper camel case
+@dataclass
+class SimulatorConfiguration:
+    freq: float
+    box_size: float
+    num_scan_steps: int
+    neighbor_radius: float
+    to_jit: bool
 
-    :param name: The string in snake case
-    :return: The string in upper camel case
-    """
-    return ''.join(word.capitalize() for word in name.split('_'))
+    @classmethod
+    def from_simulator(cls, simulator):
+        field_names = [field.name for field in fields(cls)]
+        return cls(**{
+            field_name: getattr(simulator, field_name)
+            for field_name in field_names})
+
+
+@dataclass
+class EntityTypeConfiguration:
+    name: str
+    idx: int
+    kwargs: dict
+    state_cls: Type
+
+
+@dataclass
+class EntityTypeClientConfiguration:
+    param: ConstructorConfiguration
+    simulator_controller: ConstructorConfiguration
+    notebook_controller: ConstructorConfiguration
+    panel_controller: ConstructorConfiguration
+    panel_interface: ConstructorConfiguration
+
+
+class SceneConfiguration:
+
+    def __init__(self, scene_name: str, seed=None):
+        self.scene_name = scene_name
+        self.config = load_scene_config(scene_name)
+        self._base_state_cls = import_class(self.config.state.cls)
+        self.entity_state_cls = import_class(self.config.entities.entity_state.cls)
+        self.entity_type_configs = {
+            entity_type: EntityTypeConfiguration(
+                name=entity_type,
+                idx=idx,
+                kwargs=extend_kwargs(self.config.entities[entity_type].kwargs,
+                                     self.config.entities[entity_type].kwargs.n_exists),
+                state_cls=import_class(self.config.entities[entity_type].cls)
+            )
+            for idx, entity_type in enumerate(self.config.entities.entity_types)}
+        if self.config.client != 'None':
+            self.entity_type_client_configs = {
+                entity_type: EntityTypeClientConfiguration(**{attr: ConstructorConfiguration(
+                    cls= import_class(data.cls),
+                    kwargs=extend_kwargs(data.kwargs,
+                                         self.config.entities[entity_type].kwargs.n_exists),
+                    ) for attr, data in config.items()})
+                for entity_type, config in self.config.client.items()}
+
+        self.subtype_labels = {i: label for i, label in enumerate(self.config.subtypes)}
+        self.entity_types = self.config.entities.entity_types
+        self.seed = seed
+        self.generate_missing_params()
+        self.state = self.create_state()
+
+    def generate_missing_params(self):
+        for entity_type in self.config.entities.entity_types:
+            entity_params = self.config.entities[entity_type].kwargs
+            n = entity_params.n_exists
+            if entity_params.position == 'random':
+                # Generate random positions if not provided
+                entity_params.position = generate_random_positions(n, self.config.environment.kwargs.box_size, self.seed)
+            if entity_params.orientation == 'random':
+                # Generate random orientations if not provided
+                entity_params.orientation = generate_random_orientations(n, self.seed)
+            self.config.entities[entity_type].kwargs = entity_params
+
+    def create_state_cls(self):
+        """Create the state from the scene configuration
+
+        :return: The created state
+        """
+
+        State = create_state_cls(
+            base_state_cls=self._base_state_cls,
+            entity_types=self.entity_types,
+            entity_state_cls=self.entity_state_cls,
+            entity_types_to_cls={etype: config.state_cls for etype, config in self.entity_type_configs.items()}
+        )
+
+        return State
+    
+    def create_state(self):
+
+        entity_idx_offset = 0
+        etype_instance = {}
+        entity_types_kwargs = {etype: config.kwargs for etype, config in self.entity_type_configs.items()}
+        for etype in self.entity_types:
+            cls = self.entity_type_configs[etype].state_cls
+            etype_instance[etype] = cls.create(entity_idx_offset, entity_types_kwargs, etype)
+            entity_idx_offset += self.entity_type_configs[etype].kwargs['n_exists']
+
+        state_cls = self.create_state_cls()
+        state = state_cls(**{attr: jnp.array(val) for attr, val in self.config.state.kwargs.items()},
+                        entity_state = self.entity_state_cls.create(self.entity_types, entity_types_kwargs),  #, etype_order), 
+                        **etype_instance)
+        
+        return state
+    
+    def create_environment(self):
+        env_cls = import_class(self.config.environment.cls)
+        return env_cls(state=self.state, **self.config.environment.kwargs)
+    
+    def create_simulator(self):
+        env = self.create_environment()
+        simulator_cls = import_class(self.config.simulator.cls)
+        return simulator_cls(env=env, scene_name=self.scene_name, **self.config.simulator.kwargs)
