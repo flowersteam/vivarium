74614f5daeb805d79257123c2acf4fcc2393f172
Clément Moulin-Frier <clement.moulinfrier@gmail.com>
2025-04-16 22:48:53 +0200

    Adapt particle-lenia state

--- vivarium/environments/particle_lenia/state.py
+++ vivarium/environments/particle_lenia/state.py
@@ -1,5 +1,3 @@
-from collections.abc import Iterable
-
 import jax.numpy as jnp
 
 from jax_md.dataclasses import dataclass as md_dataclass
@@ -8,7 +6,7 @@ from vivarium.environments.state import ParticleState
 
 
 @md_dataclass
-class ObjectState(ParticleState):
+class ParticleLeniaState(ParticleState):
     mu_k: jnp.array
     sigma_k: jnp.array
     w_k: jnp.array
@@ -16,9 +14,5 @@ class ObjectState(ParticleState):
     sigma_g: jnp.array
     c_rep: jnp.array
     @classmethod
-    def create(cls, entity_idx_offset, params, entity_type_field):
-        for attr, val in params['state_data'][entity_type_field]['kwargs'].items():
-            if attr in [field.name for field in cls.__dataclass_fields__.values()]:
-                if not isinstance(val, Iterable) or len(val) == 1:
-                    params['state_data'][entity_type_field]['kwargs'][attr] = [val] * params['state_data'][entity_type_field]['kwargs']['n_exists']
-        return cls._create(entity_idx_offset, params, entity_type_field)
+    def create(cls, entity_idx_offset, entity_types_kwargs, entity_type):
+        return cls._create(entity_idx_offset, entity_types_kwargs, entity_type)
