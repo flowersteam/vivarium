63fb373acac29e2bdfa256b9fe55334c669ef8ac
Clément Moulin-Frier <clement.moulinfrier@gmail.com>
2025-04-16 23:20:27 +0200

    Move some utils to converters.py. Some might be unused but keeping them, they might be handy at some point

--- vivarium/utils/converters.py
+++ vivarium/utils/converters.py
@@ -1,3 +1,5 @@
+import importlib
+import re
 import numpy as np
 import matplotlib.colors as mcolors
 
@@ -16,6 +18,7 @@ def rgb_array_to_string(rgb_array):
     '''
     return mcolors.to_hex(np.array(rgb_array))
 
+
 # Decorator to access nested fields in a class
 def access_nested_fields(field_map):
     def decorator(cls):
@@ -31,3 +34,38 @@ def access_nested_fields(field_map):
                 setattr(cls, field_name, prop)
         return cls
     return decorator
+
+
+def import_class(class_path):
+    if not isinstance(class_path, str):
+        return class_path
+    module_name, class_name = class_path.rsplit('.', 1)
+    module = importlib.import_module(module_name)
+    return getattr(module, class_name)
+
+
+def class_import_path(cls) -> str:
+    """Get the import path of a given class
+
+    :param cls: The class to get the import path for
+    :return: The import path as a string
+    """
+    return f"{cls.__module__}.{cls.__name__}"
+
+
+def upper_camel_to_snake(name: str) -> str:
+    """Convert an upper camel case string to snake case
+
+    :param name: The string in upper camel case
+    :return: The string in snake case
+    """
+    return re.sub(r'(?<!^)(?=[A-Z])', '_', name).lower()
+
+
+def snake_to_upper_camel(name: str) -> str:
+    """Convert a snake case string to upper camel case
+
+    :param name: The string in snake case
+    :return: The string in upper camel case
+    """
+    return ''.join(word.capitalize() for word in name.split('_'))
