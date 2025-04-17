9d04115d8d23bf8d4459ba2416eef5a0b02673a4
Clément Moulin-Frier <clement.moulinfrier@gmail.com>
2025-04-17 18:21:02 +0200

    Update grpc tests

--- tests/test_grpc.py
+++ tests/test_grpc.py
@@ -4,22 +4,31 @@ import jax.numpy as jnp
 from vivarium.simulator.grpc_server.converters import state_to_proto, proto_to_state, changes_to_proto, proto_to_changes
 from vivarium.controllers.dataclass_wrapper import DataclassWrapper
 from vivarium.environments.physics_engine import init_state_fn
-from vivarium.utils.scene_configs import load_scene_config
-from vivarium.environments.base_env import create_state
-from vivarium.simulator.simulator import Simulator
+from vivarium.utils.scene_configs import SceneConfiguration
+
+
+scene_config = SceneConfiguration('braitenberg')
 
 
 def get_state():
-    state = create_state(load_scene_config('braitenberg'))
+    state = scene_config.create_state()
     state = init_state_fn(random.PRNGKey(0))(state)
     return state
 
 
-def test_de_serialiazation():
+def test_state_de_serialiazation():
     state = get_state()
     p_state = state_to_proto(state)
     state_2 = proto_to_state(p_state, state.__class__)
     assert state.entity_state.position_center[0, 1] == state_2.entity_state.position_center[0, 1]
+
+def test_parameters_de_serialization():
+    simulator = scene_config.create_simulator()
+    simulator_parameters = simulator.get_simulator_parameters()
+    p_parameters = state_to_proto(simulator_parameters)
+    simulator_parameters_2 = proto_to_state(p_parameters, simulator_parameters.__class__)
+    assert simulator_parameters.freq == simulator_parameters_2.freq
+    assert simulator_parameters.box_size == simulator_parameters_2.box_size
 
 
 def test_changes():
@@ -40,7 +49,7 @@ def test_changes():
 
 
 def test_simulator_grpc():
-    simulator = Simulator.from_scene('braitenberg')
+    simulator = scene_config.create_simulator()
 
     dw = DataclassWrapper()
     dw.env.num_scan_steps = 42
@@ -58,3 +67,4 @@ def test_simulator_grpc():
     changes_2 = proto_to_changes(p_changes)
     simulator  = dw.update_state(simulator, changes_2)
     assert jnp.equal(jnp.array(42), simulator.state.entity_state.friction).all()
+
