088f6f0f7e6dc2d06cadd7c6b0a4f4d69aed1c53
Clément Moulin-Frier <clement.moulinfrier@gmail.com>
2025-04-17 19:16:08 +0200

    Update run_server, now way simpler

--- scripts/run_server.py
+++ scripts/run_server.py
@@ -1,21 +1,14 @@
 import logging
-import hydra
 
+import hydra
 from omegaconf import DictConfig, OmegaConf
 from hydra.core.hydra_config import HydraConfig
 
-from vivarium.simulator.simulator import Simulator
-from vivarium.environments.braitenberg.selective_sensing.selective_sensing_env import (
-    SelectiveSensorsEnv,
-    init_state,
-)
 from vivarium.simulator.grpc_server.simulator_server import serve
+from vivarium.utils.scene_configs import SceneConfiguration
+
 
 lg = logging.getLogger(__name__)
-
-# Define parameters of the simulator
-UPDATE_FREQ = 60
-NUM_STEPS_LAX = 6
 
 
 @hydra.main(version_base=None, config_path="../conf", config_name="config")
@@ -26,20 +19,9 @@ def main(cfg: DictConfig = None) -> None:
     hydra_cfg = HydraConfig.get()
     scene_name = OmegaConf.to_container(hydra_cfg.runtime.choices)["scene"]
     lg.info(f"Scene running: {scene_name}")
-    scene_config = OmegaConf.merge(cfg.default, cfg.scene)
 
-    # init state and environment
-    state = init_state(**scene_config)
-    env = SelectiveSensorsEnv(state=state)
-
-    # init the simulator
-    simulator = Simulator(
-        env_state=state,
-        env=env,
-        scene_name=scene_name,
-        update_freq=UPDATE_FREQ,
-        num_steps_lax=NUM_STEPS_LAX,
-    )
+    # Create the simulator
+    simulator = SceneConfiguration(scene_name).create_simulator()
 
     # start and host the simulator on a server
     serve(simulator)
