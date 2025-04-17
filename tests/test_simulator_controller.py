94d0d9e8dc246cfb004be564bfa8756e2d5feb4e
Clément Moulin-Frier <clement.moulinfrier@gmail.com>
2025-04-17 22:26:19 +0200

    Update simulator controller tests

--- tests/test_simulator_controller.py
+++ tests/test_simulator_controller.py
@@ -1,41 +1,37 @@
 import jax.numpy as jnp
-from vivarium.environments.braitenberg.selective_sensing.selective_sensing_env import (
-    init_state,
-    SelectiveSensorsEnv,
-    EntityType
-)
-from vivarium.simulator.simulator import Simulator
+
+from vivarium.environments.braitenberg.selective_sensing.selective_sensing_env import AgentState
 from vivarium.controllers.simulator_controller import SimulatorController, ControllerEntity
+from vivarium.environments.braitenberg.selective_sensing.state import AgentState
+from vivarium.utils.scene_configs import SceneConfiguration
 
 NUM_STEPS = 10
 
 def test_base_entity():
-    state = init_state()
-    entity = ControllerEntity(state, 0, EntityType.AGENT)
+    config = SceneConfiguration('braitenberg')
+    state = config.create_state()
+    entity = ControllerEntity(state, 0, config.entity_types[0])
     
     entity.x_position = 10
     entity.apply_to_state(state)
 
 
 def test_simulator_controller():
-    """Test default simulator run"""
-    state = init_state()
-    env = SelectiveSensorsEnv(state=state)
-    simulator = Simulator(env_state=state, env=env)
+    simulator = SceneConfiguration('braitenberg').create_simulator()
     controller = SimulatorController(simulator)
     controller.step()
 
     idx = 0
     pos = controller.state.entity_state.position_center[idx]
 
-    ag =controller.agents[idx]
+    ag = controller.agents[idx]
     assert (jnp.equal(pos, ag.position_center).all())
 
-    ag.behavior = [3, 2]
+    ag.behavior = [3, 2, 1, 5]
     controller.apply_changes()
     controller.update_state()
 
-    assert jnp.equal(jnp.array([3, 2]), controller.state.agent_state.behavior[idx]).all()
+    assert jnp.equal(jnp.array([3, 2, 1, 5]), controller.state.field(AgentState).behavior[idx]).all()
 
     for ag in controller.agents:
         ag.behavior = 5
@@ -46,10 +42,10 @@ def test_simulator_controller():
         controller.step()
         assert (not jnp.equal(pos, ag.position_center).all())
 
-    controller.simulator_state.freq = -10
+    controller.simulator_parameters.freq = -10
+    controller.simulator_parameters.box_size = 42.
     controller.apply_changes()
 
-    assert controller.state.simulator_state.freq != -10
-    controller.update_state()
-    assert controller.state.simulator_state.freq == -10
-
+    assert controller.client.freq == -10
+    assert controller.client.box_size == 42.
+    assert controller.client.env.box_size == 42.
