312bca445b625f094681ce5a09009908aefc2341
Clément Moulin-Frier <clement.moulinfrier@gmail.com>
2025-07-02 14:13:08 +0200

    Adapt run_server

--- scripts/run_server.py
+++ scripts/run_server.py
@@ -1,11 +1,10 @@
 import logging
 
-import hydra
 from omegaconf import DictConfig, OmegaConf
-from hydra.core.hydra_config import HydraConfig
+import hydra
 
-from vivarium.simulator.grpc_server.simulator_server import serve
 from vivarium.utils.scene_configs import SceneConfiguration
+from vivarium.simulator.grpc_server.simulator_server import serve
 
 
 lg = logging.getLogger(__name__)
@@ -13,15 +12,15 @@ lg = logging.getLogger(__name__)
 
 @hydra.main(version_base=None, config_path="../conf", config_name="config")
 def main(cfg: DictConfig = None) -> None:
+    
+    print(OmegaConf.to_yaml(cfg))
+    
     logging.basicConfig(level=cfg.log_level)
 
-    # retrieve args from config
-    hydra_cfg = HydraConfig.get()
-    scene_name = OmegaConf.to_container(hydra_cfg.runtime.choices)["scene"]
-    lg.info(f"Scene running: {scene_name}")
+    lg.info(f"Scene running: {cfg.scene.scene_name}")
 
     # Create the simulator
-    simulator = SceneConfiguration(scene_name).create_simulator()
+    simulator = SceneConfiguration(cfg.scene).create_simulator()
 
     # start and host the simulator on a server
     serve(simulator)
