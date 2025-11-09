7cfa40fa5f93b8f535ac9b81ee6aa8f662a982eb
Clément Moulin-Frier <clement.moulinfrier@gmail.com>
2025-11-10 00:30:38 +0100

    Refactor Simulator, in particular for being able to run simulation from client in a cleaner way

--- vivarium/controllers/simulator_controller.py
+++ vivarium/controllers/simulator_controller.py
@@ -1,14 +1,14 @@
 import math
-import logging
 import hydra
+import logging
 import threading
 from dataclasses import asdict, fields
 
 from vivarium.utils.handle_server_interface import start_server_and_interface, stop_server_and_interface
 from vivarium.simulator.grpc_server.simulator_client import SimulatorGRPCClient
-from vivarium.controllers.panel_controller import SimulatorParametersWrapper
-
 from vivarium.utils.scene_configs import load_scene_config
+from vivarium.controllers.dataclass_wrapper import Remote
+from vivarium.utils.timer import sleep_timer
 
 
 logging.basicConfig(level=logging.INFO)
@@ -17,44 +17,44 @@ lg = logging.getLogger(__name__)
 
 def start_session(scene_name, run=True):
     start_server_and_interface(cmd_args=[f'scene={scene_name}'])
-    scene_config = load_scene_config(scene_name)
-    components_config = scene_config.environment.components
-    controller = SimulatorController.from_config(config=components_config, run_from_server=scene_config.simulator.run_from_server)
+    controller = SimulatorController.from_client()
+    controller.simulator.run_from = controller.client.name
     if run:
-        controller.run()
+        controller.simulator.simulation_running = True
+        controller.apply_changes()
     return controller
 
 class SimulatorController:
 
-    def __init__(self, client=None, run_from_server=True, subtypes=[], **controllers):
+    def __init__(self, client=None, subtypes=[], **controllers):
         self.client = client or SimulatorGRPCClient()
-        self.state = self.client.state
-        self.simulator_parameters = self.client.get_simulator_parameters()
+        self.state = self.client.get_state()
         self.subtype_labels = {i: label for i, label in enumerate(subtypes)}
         
         self.controllers = controllers
         
         self.time = 0
-        self.run_from_server = run_from_server
         self._is_running = False
         
-        self.create_simulator_parameters_wrapper()
+        cp = self.client.get_controller_parameters()
+        self.controllers['simulator'] = Remote(cp.simulator, path=('controller_parameters', 'simulator'))
 
     @classmethod
-    def from_config(cls, config, client=None, run_from_server=True):
+    def from_client(cls, client=None):
         client = client or SimulatorGRPCClient()
-        state = client.state
-        cp = asdict(client.controller_parameters)
+        scene_config = load_scene_config(client.scene_name)
+        components_config = scene_config.environment.components       
+        state = client.get_state()
+        cp = asdict(client.get_controller_parameters())
         controllers = {}
-        for name, c_config in config.component_list.items():
+        for name, c_config in components_config.component_list.items():
             if 'client' in c_config and 'controller_cls' in c_config.client:
                 c_cls = hydra.utils.get_class(c_config.client.controller_cls)
                 p = {} if name not in cp or cp[name] is None else cp[name]
-                controllers[name] = c_cls.from_config(name, c_config.client, state, notebook_control=not run_from_server, **p)
+                controllers[name] = c_cls.from_config(name, c_config.client, state, **p)
         return cls(
             client=client,
-            run_from_server=run_from_server,
-            subtypes=config.subtype_labels,
+            subtypes=components_config.subtype_labels,
             **controllers
         )
 
@@ -63,37 +63,29 @@ class SimulatorController:
             return self.controllers[name]
         raise AttributeError(f"'SimulatorController' object has no attribute '{name}'")
 
-    def create_simulator_parameters_wrapper(self):
-        self.simulator_parameters = SimulatorParametersWrapper(self.simulator_parameters)
-
     def run(self, threaded=True, num_steps=math.inf, debug_mode=False):
         """
-        Run the simulation.
-        :param threaded: if self.run_from_server is False, whether to run the simulation in a thread or not, defaults to True
-        :param num_steps: if self.run_from_server is False, num_steps, defaults to math.inf
+        Execute the simulation loop from this client.
+        :param threaded: Whether to run the simulation in a thread or not, defaults to True
         :raises RuntimeError: if the simulator is already started
         """
         if self.is_running():
             lg.info("Simulator is already started")
             return
-        if self.run_from_server:
-            self.client.start()
-        else:
+
         # automatically catch errors only if not in debug mode
-            catch_errors = not debug_mode
-            if self._is_running:
-                print("Simulator is already started")
-                return
-            self._is_running = True
-            if threaded:
-                run_thread = threading.Thread(
-                    target=self._run, args=(num_steps, catch_errors)
-                )
-                run_thread.daemon = True
-                run_thread.start()
-            else:
-                self._run(num_steps=num_steps, catch_errors=catch_errors)
+        catch_errors = not debug_mode
+        
         self._is_running = True
+        if threaded:
+            run_thread = threading.Thread(
+                target=self._run, args=(num_steps, catch_errors)
+            )
+            run_thread.daemon = True
+            run_thread.start()
+        else:
+            self._run(num_steps=num_steps, catch_errors=catch_errors)
+        lg.info("Simulator started on client")
             
     def _run(self, num_steps=math.inf, catch_errors=True):
         """run the simulation for a given number of steps
@@ -106,28 +98,25 @@ class SimulatorController:
         while run_time < num_steps and self._is_running:
             # self.execute_routines_and_behaviors(catch_errors=catch_errors)
 
-            self.step()
+            with sleep_timer(freq=self.controllers['simulator'].freq):
+                self.step()
 
-            self.time += 1
-            run_time += 1
+                self.time += 1
+                run_time += 1
 
         # finally stop the simulation
-        self.stop()
+        if self.is_running():
+            self.stop()
 
 
     def stop(self):
-        """Stop the simulator."""
+        """Stop simulation loop on this client."""
         if not self.is_running():
             lg.info("Simulator is already stopped")
-        else:
-            if self.run_from_server:
-                self.client.stop()
         self._is_running = False
 
     def is_running(self):
-        """Check if the simulator is started."""
-        if self.run_from_server:
-            return self.client.is_running()
+        """Check if the simulation loop is started on this client."""
         return self._is_running
 
     def step(self):
@@ -148,6 +137,14 @@ class SimulatorController:
             if controller_parameters is not None:
                 if name in cp_fields:
                     controller.set_controller_parameters(getattr(controller_parameters, name))
+        if self.simulator.run_from == self.client.name:
+            if self.is_running() != self.simulator.simulation_running:
+                if self.simulator.simulation_running:
+                    self.run(threaded=True)
+                else:
+                    self.stop()
+        elif self.is_running():
+            self.stop()
 
     def update_state(self):
         """Update the state from server to client."""
@@ -161,17 +158,14 @@ class SimulatorController:
             change = controller.fetch_changes()
             changes.extend(change)
 
-        change = self.simulator_parameters.fetch_changes()
-        if change:
-            changes.extend([change])
         return changes
 
-    def apply_changes(self, changes=None):
+    def apply_changes(self, changes=None): # TODO: should this be in SimulatorClient instead?
         changes = changes or self.fetch_changes()
         if len(changes) > 0:
             cp = self.client.apply_changes(changes)
         else:
-            cp = self.client.controller_parameters
+            cp = self.client.get_controller_parameters()
         self.update_controllers(controller_parameters=cp)
             
     def stop_session(self, safe_mode=False):
@@ -179,3 +173,4 @@ class SimulatorController:
         if self._is_running:
             self.stop()
         stop_server_and_interface(safe_mode=safe_mode)
+        
