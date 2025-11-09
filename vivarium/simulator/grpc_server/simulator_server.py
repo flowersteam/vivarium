8b36c6838c54699e08860f4f4318bab96ca11a7b
Clément Moulin-Frier <clement.moulinfrier@gmail.com>
2025-11-10 00:14:16 +0100

    imports

--- vivarium/simulator/grpc_server/simulator_server.py
+++ vivarium/simulator/grpc_server/simulator_server.py
@@ -5,19 +5,17 @@ from collections import defaultdict
 from contextlib import contextmanager
 
 
+from numproto.numproto import proto_to_ndarray
 import simulator_pb2_grpc
 import simulator_pb2
 import grpc
 
-from numproto.numproto import proto_to_ndarray
 
 from vivarium.simulator.grpc_server.converters import dataclass_to_proto, proto_to_dataclass, proto_to_changes
-from vivarium.simulator.config import SimulatorConfiguration
 
 
 lg = logging.getLogger(__name__)
 Empty = simulator_pb2.google_dot_protobuf_dot_empty__pb2.Empty
-
 
 
 @contextmanager
