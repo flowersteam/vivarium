c23dd381d1549d22ffba16818406f3152ce2382c
Clément Moulin-Frier <clement.moulinfrier@gmail.com>
2025-11-10 21:35:43 +0100

    Add behavior management in AgentController (previously was in AgentNotebookController, which is now unsused)

--- vivarium/environment/components/entities/braitenberg/controller.py
+++ vivarium/environment/components/entities/braitenberg/controller.py
@@ -2,7 +2,6 @@ import numpy as np
 
 from vivarium.controllers.utils import BehaviorHandler, Logger
 from vivarium.environment.components.entities.controller import EntityController
-from vivarium.environment.components.entities.controller import NotebookControllerEntity
 from vivarium.environment.components.entities.controller import EntityListController
 from vivarium.environment.components.entities.braitenberg.behaviors import Behaviors, behavior_params
 
@@ -56,7 +55,10 @@ class BehaviorController:
             
 class AgentController(EntityController):       
 
-
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        object.__setattr__(self, 'behavior_handler', BehaviorHandler())
+        
     @property
     def behaviors(self):
         return BehaviorController(self)
@@ -69,77 +71,6 @@ class AgentController(EntityController):
         """Return the proximeters values of the agent"""
         #TODO: implement sensed_entities filtering (see AgentNotebookController)
         return self.prox.tolist()
-
-
-class AgentNotebookController(NotebookControllerEntity, AgentController):
-    """Agent class that represents an agent in the simulation
-    """
-
-    def __init__(self, *args, **kwargs):
-        super().__init__(*args, **kwargs)
-        object.__setattr__(self, 'behavior_handler', BehaviorHandler())
-        object.__setattr__(self, 'logger', Logger())
-        object.__setattr__(self, 'eating_range', 10)
-        object.__setattr__(self, 'diet', [])
-        object.__setattr__(self, 'ate', False)
-        object.__setattr__(self, 'time_since_feeding', np.inf)
-        object.__setattr__(self, 'simulation_entities', None)
-        # self.set_manual()
-
-    def set_manual(self):
-        """Set the agent's behavior to manual"""
-        self.behavior = np.full(
-            shape=self.behavior.shape, fill_value=Behaviors.MANUAL.value
-        )
-        self.stop_motors()
-
-    def sensors(self, sensed_entities=None):
-        """Return the sensors values of the agent
-
-        :param sensed_entities: sensed_entities of the sensors under the form of strings, defaults to None
-        :return: sensors values
-        """
-        left, right = self.prox
-        if sensed_entities is not None:
-            # TODO: transform the strings of sensed entities into ints (this fn can surely be optimized)
-            assert all(
-                ent_subtype in self.valid_subtypes for ent_subtype in sensed_entities
-            ), f"Please specify valid sensed entities among {self.valid_subtypes}"
-            sensed_entities = [
-                self._subtype_label_to_idx[label] for label in sensed_entities
-            ]
-            sensed_type_left, sensed_type_right = self.prox_sensed_ent_type
-            left = left if sensed_type_left in sensed_entities else 0
-            right = right if sensed_type_right in sensed_entities else 0
-        return [left, right]
-
-    def entity_sensors(self):
-        """Return the left and right sensed entities of the agent if they are sensed, else None
-
-        :return: sensed entities
-        """
-        left_idx, right_idx = self.prox_sensed_ent_idx
-        left_ent = (
-            self.simulation_entities[left_idx] if self.config.left_prox != 0 else None
-        )
-        right_ent = (
-            self.simulation_entities[right_idx] if self.config.right_prox != 0 else None
-        )
-        return [left_ent, right_ent]
-
-    def attribute_sensors(self, sensed_attribute, default_value=None):
-        """Return the sensed attribute of the left and right sensed entities
-
-        :param sensed_attribute: sensed_attribute
-        :param default_value: default value if the attribute is not found, defaults to None
-        :return: sensed attributes
-        """
-        left_ent, right_ent = self.entity_sensors()
-        # get the sensed attribute of the entities with a getattr, specify a default value if the attribute is not found
-        return (
-            getattr(left_ent, sensed_attribute, default_value),
-            getattr(right_ent, sensed_attribute, default_value),
-        )
 
     def attach_behavior(
         self, behavior_fn, name=None, interval=1, weight=1.0, start=True
@@ -217,51 +148,6 @@ class AgentNotebookController(NotebookControllerEntity, AgentController):
         """
         self.behavior_handler.behave(self, time)
         # increment time since last meal for all alive agents
-        object.__setattr__(self, 'time_since_feeding', self.time_since_feeding + 1)
-
-    def stop_motors(self):
-        """Stop the motors of the agent"""
-        self.motor = [0, 0]
-
-    def has_eaten(self):
-        """Check if the agent has eaten
-
-        :return: True if the agent has eaten, False otherwise
-        """
-        val = self.ate
-        self.ate = False
-        return val
-
-    # TODO : maybe delete this function
-    def has_eaten_since(self, time):
-        """Check if the agent has eaten since a given time
-
-        :return: True if the agent has eaten since the given time, False otherwise
-        """
-        return self.time_since_feeding <= time
-
-    def add_log(self, log_field, data):
-        """Add a log to the agent's logger (e.g robot.add_log("left_prox", left_prox_value))
-
-        :param log_field: log_field of the log
-        :param data: data logged
-        """
-        self.logger.add(log_field, data)
-
-    def get_log(self, log_field):
-        """Get the log of the agent's logger for a specific log_field
-
-        :param log_field: desired log_field
-        :return: associated log_field data
-        """
-        return self.logger.get_log(log_field)
-
-    def clear_all_logs(self):
-        """Clear all logs of the agent's logger
-
-        :return: cleared logs
-        """
-        return self.logger.clear()
 
     def print_infos(self, full_infos=False):
         """Print the agent's infos
@@ -314,7 +200,7 @@ class BraitenbergController(EntityListController):
             entity_type=entity_type,
             state=state,
             subtype_labels=subtype_labels,
-            controller_cls=AgentController if not notebook_control else AgentNotebookController,
+            controller_cls=AgentController,
             notebook_control=notebook_control,
             **kwargs
         )
