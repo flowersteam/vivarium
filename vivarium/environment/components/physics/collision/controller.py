817c5a094833d4638d2db61da635976df0ffc558
Clément Moulin-Frier <clement.moulinfrier@gmail.com>
2025-11-14 15:40:22 +0100

    minor

--- vivarium/environment/components/physics/collision/controller.py
+++ vivarium/environment/components/physics/collision/controller.py
@@ -6,7 +6,7 @@ from .....controllers.controller import Controller
 class CollisionController(Controller):
 
     def __getattr__(self, attr):
-        return getattr(self._remote.state, f'{self._name}_state').__getattr__(attr).obj()
+        return getattr(self._remote.state, f'{self._name}_state').__getattr__(attr)
     
     def __setattr__(self, attr, value):
         if attr.startswith('_'):
