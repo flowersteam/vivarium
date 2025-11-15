4da71383a49ca732de2a6eac40f363eeec033473
Clément Moulin-Frier <clement.moulinfrier@gmail.com>
2025-11-15 19:26:52 +0100

    minor import

--- vivarium/environment/components/eco_evo/__init__.py
+++ vivarium/environment/components/eco_evo/__init__.py
@@ -1,3 +1,4 @@
 from .spawn.component import SpawnComponent
 from .spawn.controller import SpawnController
+from .spawn.interface import SpawnInterface
 from .component import ConsumptionComponent, EnergyComponent, ReproductionComponent
