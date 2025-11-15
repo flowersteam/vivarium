b0223009c64a1617379b8427aab968bc372bd390
Clément Moulin-Frier <clement.moulinfrier@gmail.com>
2025-11-15 19:25:11 +0100

    Improve SpawnController

--- vivarium/environment/components/eco_evo/spawn/controller.py
+++ vivarium/environment/components/eco_evo/spawn/controller.py
@@ -1,13 +1,43 @@
-from .....controllers.controller import Controller
+import numpy as np
+
+from .....controllers.controller import Controller, AttributeMapping, set_nested_attr
 
 
 class SpawnController(Controller):
+    def __init__(self, name, remote, mapping=None):
+        
+        self._subtype_labels = remote.controller_parameters.simulator.subtype_labels.obj()
+        
+        mapping = mapping or {
+            'position_range': AttributeMapping(
+                f'state.{name}_state.position_range',
+                remote_to_ctrl_fn=lambda x: tuple(x.tolist()),
+                ctrl_to_remote_fn=lambda x: np.array(x)
+            ),
+            'orientation_range': AttributeMapping(
+                f'state.{name}_state.orientation_range',
+                remote_to_ctrl_fn=lambda x: tuple(x.tolist()),
+                ctrl_to_remote_fn=lambda x: np.array(x)
+            ),
+            'subtype': AttributeMapping(
+                f'state.{name}_state.subtype',
+                remote_to_ctrl_fn=lambda x: self._subtype_labels[x],
+                ctrl_to_remote_fn=lambda x: np.array(self._subtype_labels.index(x), dtype=int)
+            ),
+        }
+
+        super().__init__(name, remote, mapping=mapping)
 
     def __getattr__(self, attr):
-        return getattr(self._remote.state, f'{self._name}_state').__getattr__(attr).obj()
+        if self.to_deal_with(attr):
+            return getattr(self._remote.state, f'{self._name}_state').__getattr__(attr)#obj()
+        else:            
+            return super().__getattr__(attr)
     
     def __setattr__(self, attr, value):
-        if attr.startswith('_'):
-            object.__setattr__(self, attr, value)
+        if self.to_deal_with(attr):
+            attr = f'state.{self._name}_state.{attr}'
+            set_nested_attr(self._remote, attr, value)
         else:
-            getattr(self._remote.state, f'{self._name}_state').__setattr__(attr, value)
+            super().__setattr__(attr, value)
+            
