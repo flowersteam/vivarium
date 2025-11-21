96908e9c2cb7983af5ea021a09c2db2b013e5cf8
Clément Moulin-Frier <clement.moulinfrier@gmail.com>
2025-11-21 17:40:19 +0100

    Good enough version of setup.py for now

--- setup.py
+++ setup.py
@@ -1,11 +1,12 @@
 from setuptools import setup, find_packages
-from setuptools.command.develop import develop
-from setuptools.command.install import install
-import subprocess
-import sys
+
+# TODO: clean commented lines at some point.
+# Haven't done it yet in case something messes in the compatibility between jax-md and the last version of jax
+# In that case, we might need to first install a specific version of jax,
+# then install jax-md from without its dependencies, and finally install jax-md dependencies manually
 
 # Specific versions/commits
-JAX_VERSION = "0.7.2"  # Replace with your desired version
+# JAX_VERSION = "0.7.2"  # Replace with your desired version
 JAX_MD_COMMIT = "6bd17d29ce5f9fe35a5582a42a9973b1ecd0859f"  # Replace with your specific commit hash
 JAX_MD_URL = f"jax-md @ git+https://github.com/jax-md/jax-md.git@{JAX_MD_COMMIT}"
 
@@ -24,15 +25,6 @@ JAX_MD_URL = f"jax-md @ git+https://github.com/jax-md/jax-md.git@{JAX_MD_COMMIT}
 #     "frozendict",
 #     "pymatgen",
 # ]
-
-# def install_jax_md():
-#     """Install JAX-MD from GitHub without dependencies."""
-#     jax_md_url = f"git+https://github.com/jax-md/jax-md.git@{JAX_MD_COMMIT}"
-#     print(f"\nInstalling JAX-MD from commit {JAX_MD_COMMIT} (without deps)...")
-#     subprocess.check_call([
-#         sys.executable, "-m", "pip", "install", 
-#         "--no-deps", jax_md_url
-#     ])
 
 
 setup(
@@ -54,7 +46,8 @@ setup(
         "panel==1.8.3",
         "param==2.2.1",
         "hydra-core==1.3.2",
-        "psutil"
+        "psutil",
+        "pytest"
         # Add other dependencies here
     ], # + JAX_MD_DEPS,
     
@@ -77,33 +70,3 @@ setup(
     python_requires=">=3.10",
     description="Vivarium enables configuring and running large-scale multi-agents simulations using Jax, with real-time interactions.",
 )
-
-
-
-
-# import io
-# import os
-# import setuptools
-
-# # https://packaging.python.org/guides/making-a-pypi-friendly-readme/
-# this_directory = os.path.abspath(os.path.dirname(__file__))
-# with io.open(os.path.join(this_directory, "README.md"), encoding="utf-8") as f:
-#     long_description = f.read()
-
-# setuptools.setup(
-#     name="vivarium",
-#     version="0.1.0",
-#     license="MIT",
-#     author="Clément Moulin-Frier",
-#     author_email="clement.moulinfrier@gmail.com",
-#     packages=setuptools.find_packages(),
-#     classifiers=[
-#         "Programming Language :: Python :: 3.10",
-#         # 'License :: OSI Approved :: Apache Software License',
-#         "Operating System :: MacOS",
-#         "Operating System :: POSIX :: Linux",
-#         "Topic :: Scientific/Engineering",
-#         "Intended Audience :: Science/Research",
-#         "Intended Audience :: Developers",
-#     ],
-# )
