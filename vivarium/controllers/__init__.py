082ba0d8f33a5c89194db884b9e5488de7cb8389
Clément Moulin-Frier <clement.moulinfrier@gmail.com>
2025-11-23 15:51:16 +0100

    import

--- vivarium/controllers/__init__.py
+++ vivarium/controllers/__init__.py
@@ -1,3 +1,3 @@
-from .vivarium_controller import VivariumController
+from .vivarium_controller import VivariumController, start_session
 from .notebook_controller import NotebookController
 from .controller import Controller, AttributeMapping, set_nested_attr
