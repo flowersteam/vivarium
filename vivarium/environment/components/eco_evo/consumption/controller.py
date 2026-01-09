8103ba0c5917a5cde87888b412fbf9aec57d06ee
Clément Moulin-Frier <clement.moulinfrier@gmail.com>
2026-01-09 14:48:29 +0100

    Adapt ConsumptionController and ConsumptionInterface to last changes in ConsumptionComponent

--- vivarium/environment/components/eco_evo/consumption/controller.py
+++ vivarium/environment/components/eco_evo/consumption/controller.py
@@ -1,29 +1,47 @@
 import numpy as np
 
-from vivarium.controllers import Controller, AttributeMapping, set_nested_attr
+from vivarium.controllers import Controller, set_nested_attr
+
+class SingleConsumptionController:
+    def __init__(self, remote, idx, global_controller_name, subtype_labels):
+        self._remote = remote
+        self._idx = idx
+        self._global_controller_name = global_controller_name
+        self._subtype_labels = subtype_labels
+        
+    def __getattr__(self, attr):
+        if attr.startswith('_'):
+            return object.__getattr__(self, attr)
+        value = getattr(self._remote.state, f'{self._global_controller_name}_state').__getattr__(attr)[self._idx].item()
+        if attr == 'source_subtype' or attr == 'target_subtype':
+            return self._subtype_labels[value]
+        return value
+    
+    def __setattr__(self, attr, value):
+        if attr.startswith('_'):
+            object.__setattr__(self, attr, value)
+        else:
+            if attr == 'source_subtype' or attr == 'target_subtype':
+                value = np.array(self._subtype_labels.index(value), dtype=int)
+            self._remote.state.__getattr__(f'{self._global_controller_name}_state').__getattr__(attr)[self._idx] = value
+
 
 class ConsumptionController(Controller):
     
     def __init__(self, name, remote, mapping={}):
         self._subtype_labels = remote.controller_parameters.simulator.subtype_labels.obj()
         
-        mapping = mapping or {
-            'source_subtype': AttributeMapping(
-                f'state.{name}_state.source_subtype',
-                remote_to_ctrl_fn=lambda x: self._subtype_labels[x],
-                ctrl_to_remote_fn=lambda x: np.array(self._subtype_labels.index(x), dtype=int)
-            ),
-            'target_subtype': AttributeMapping(
-                f'state.{name}_state.target_subtype',
-                remote_to_ctrl_fn=lambda x: self._subtype_labels[x],
-                ctrl_to_remote_fn=lambda x: np.array(self._subtype_labels.index(x), dtype=int)
-            ),
-        }        
+        self._consumption_names = remote.controller_parameters.consumption.names.obj()
+        
+        self._single_consumption_controllers = {
+            c_name: SingleConsumptionController(remote, idx, name, self._subtype_labels) for idx, c_name in enumerate(self._consumption_names)
+        }
+        
         super().__init__(name, remote, mapping=mapping)
 
     def __getattr__(self, attr):
         if self.to_deal_with(attr):
-            return getattr(self._remote.state, f'{self._name}_state').__getattr__(attr)
+            return self._single_consumption_controllers[attr]
         else:            
             return super().__getattr__(attr)
     
--- vivarium/environment/components/eco_evo/consumption/interface.py
+++ vivarium/environment/components/eco_evo/consumption/interface.py
@@ -5,7 +5,7 @@ from vivarium.controllers.panel_controller import ParameterizedData
 from vivarium.environment.components.interface import Interface
 
 
-class ConsumptionParam(ParameterizedData):
+class SingleConsumptionParam(ParameterizedData):
     source_subtype = param.Selector()
     target_subtype = param.Selector()
     range = param.Number()
@@ -15,6 +15,22 @@ class ConsumptionParam(ParameterizedData):
         super().__init__(controller=controller, **params)
         self.param.source_subtype.objects = controller._subtype_labels
         self.param.target_subtype.objects = controller._subtype_labels
+
+class ConsumptionParam(ParameterizedData):
+    
+    def __init__(self, controller):
+        super().__init__(controller=controller)
+        self._single_consumption_params = [
+            SingleConsumptionParam(controller=single_controller, name=name)
+            for name, single_controller in controller._single_consumption_controllers.items()
+        ]
+        self.param.add_parameter(
+            'consumption',
+            param.Selector(
+                objects=self._single_consumption_params,
+                default=self._single_consumption_params[0]
+            )
+        )
 
 
 class ConsumptionInterface(Interface):
