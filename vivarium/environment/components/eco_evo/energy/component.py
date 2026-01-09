a0454ccd266af11a1e93d186493dcb33a12f99c7
Clément Moulin-Frier <clement.moulinfrier@gmail.com>
2026-01-09 14:50:56 +0100

    Refactor EnergyComponent to store its state variables in EnergyState

--- vivarium/environment/components/eco_evo/energy/component.py
+++ vivarium/environment/components/eco_evo/energy/component.py
@@ -6,9 +6,18 @@ from vivarium.environment.components.component import Component
 from vivarium.environment.utils import type_mask
 
 
+@md_dataclass
+class EnergyState:
+    energy: jnp.ndarray = None
+    energy_init: jnp.ndarray = None
+    energy_max: jnp.ndarray = None
+    energy_burst: jnp.ndarray = None
+    energy_decay: jnp.ndarray = None
+            
+
 class EnergyComponent(Component):
     def __init__(self, name, precedence,
-                 entity_type, subtype,
+                 entity_type,
                  energy_init, energy_max, energy_decay, 
                  energy_burst):
         super().__init__(name, precedence)
@@ -18,98 +27,81 @@ class EnergyComponent(Component):
         self.energy_burst = jnp.array(energy_burst)
         self.energy_decay = jnp.array(energy_decay)
         self.entity_type = entity_type
-        self.subtype = subtype
+        self.state_attr = f'{self.name}_state'
 
     def get_step_function(self, state, neighbor_manager, key):
 
-        entity_type = state.entity_type_to_int(self.entity_type)
-        idxs = state.e_cond(self.entity_type)
         n_entities = state.entity_state.exists.shape[0]
 
         def state_fn(state, neighbors, key):
-            entities = getattr(state, self.entity_type)
 
-            mask = type_mask(state.entity_state, entity_type=entity_type, subtype=self.subtype)
+            energy_state = getattr(state, self.state_attr)
             
-            consumption_matrix = state.entity_state.consumption_matrix
-            # Normalize mask by row sums, handling zero-sum rows
-            row_sums = consumption_matrix.sum(axis=1, keepdims=True)
-            mask_normalized = jnp.where(row_sums > 0, consumption_matrix / row_sums, 0)            
+            target_mask = type_mask(state.entity_state)           
+            
+            consumption_matrix = state.consumption_state.consumption_matrix
 
             # # `consuming` is the number of other entities consumed by each entity.
             # # It is a float, so that when multiple entities consume the same target,
             # # they only get the corresponding fraction of it.
-            consuming = mask_normalized.sum(axis=1)
+            consuming = consumption_matrix.sum(axis=1)
             
             neigh_flat = neighbors.idx.ravel()
-            mask_normalized_flat = mask_normalized.ravel()
+            consumption_matrix_flat = consumption_matrix.ravel()
             
             # Similar logic as in consuming
-            consumed = jax.ops.segment_sum(mask_normalized_flat, neigh_flat, n_entities)            
+            consumed = jax.ops.segment_sum(consumption_matrix_flat, neigh_flat, n_entities)            
             
-            
-            energy = entities.energy + (consuming - consumed) * entities.energy_burst
+            energy = energy_state.energy + (consuming - consumed) * energy_state.energy_burst
             
             energy = jnp.where(
-                mask,
-                energy - entities.energy_decay,
+                target_mask,
+                energy - energy_state.energy_decay,
                 energy
             )
 
             # Redistribute the extra energy (positive or negative)
             # from entities outside bounds to those within bounds
-            energy_outside_bounds = (energy <= 0) | (energy > entities.energy_max)
+            energy_outside_bounds = (energy <= 0) | (energy > energy_state.energy_max)
             bounds = jnp.zeros_like(energy)
-            bounds = jnp.where(energy > entities.energy_max, entities.energy_max, bounds)            
-            extra_energy = jnp.where(mask & energy_outside_bounds, energy - bounds, 0.).sum()
-            per_entity = extra_energy / jnp.sum(mask & ~energy_outside_bounds)             
+            bounds = jnp.where(energy > energy_state.energy_max, energy_state.energy_max, bounds)            
+            extra_energy = jnp.where(target_mask & energy_outside_bounds, energy - bounds, 0.).sum()
+            per_entity = extra_energy / jnp.sum(target_mask & ~energy_outside_bounds)             
             energy = jnp.where(
-                mask & ~energy_outside_bounds,
+                target_mask & ~energy_outside_bounds,
                 energy + per_entity,
                 energy
             )
             energy = jnp.where(
-                energy_outside_bounds,
-                jnp.clip(energy, 0, entities.energy_max),
+                target_mask & energy_outside_bounds,
+                jnp.clip(energy, 0, energy_state.energy_max),
                 energy,
-            )                                  
-            
-            return state.set(**{
-                self.entity_type: entities.set(
-                    energy=energy
-                ),
-                'entity_state': state.entity_state.set(
-                    consumption_matrix=jnp.full(neighbor_manager.neighbors.idx.shape, False)
-                    )
-                }
             )
+            
+            return state.set(
+                    **{self.state_attr: getattr(state, self.state_attr).set(
+                        energy=energy
+                    )}
+                )
 
         return state_fn
 
     def update_state_cls(self, state_cls):
-        superclass = state_cls.__annotations__[self.entity_type] if self.entity_type in state_cls.__annotations__ else object
-        @md_dataclass
-        class AgentState(superclass):
-            energy: jnp.ndarray = None
-            energy_init: jnp.ndarray = None
-            energy_max: jnp.ndarray = None
-            energy_burst: jnp.ndarray = None
-            energy_decay: jnp.ndarray = None
-        state_cls.__annotations__[self.entity_type] = AgentState
+        state_cls.__annotations__[self.state_attr] = EnergyState
+        setattr(state_cls, self.state_attr, None)    
         return state_cls
 
     def init_state_fn(self, state, neighbor_manager, key):
         if len(self.energy_init.shape) == 0:
-            energy = jnp.full(getattr(state, self.entity_type).count(), self.energy_init)
+            energy = jnp.full(state.entity_state.count(), self.energy_init)
         else:
             energy = self.energy_init
         return state.set(
-            **{self.entity_type: getattr(state, self.entity_type).set(
+            **{self.state_attr: EnergyState(
                 energy=energy,
                 energy_init=self.energy_init,
                 energy_max=self.energy_max,
                 energy_burst=self.energy_burst,
-                energy_decay=self.energy_decay,                
-            )}
-        )
+                energy_decay=self.energy_decay,  
+            )})
         
