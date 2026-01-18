cd9503cd1d84d793f6699dca1fa397bce638c89a
Clément Moulin-Frier <clement.moulinfrier@gmail.com>
2026-01-18 13:53:29 +0100

    Pin last versions of dependencies in setup.py and clean it

--- setup.py
+++ setup.py
@@ -1,72 +1,32 @@
 from setuptools import setup, find_packages
 
-# TODO: clean commented lines at some point.
-# Haven't done it yet in case something messes in the compatibility between jax-md and the last version of jax
-# In that case, we might need to first install a specific version of jax,
-# then install jax-md from without its dependencies, and finally install jax-md dependencies manually
-
-# Specific versions/commits
-# JAX_VERSION = "0.7.2"  # Replace with your desired version
-JAX_MD_COMMIT = "6bd17d29ce5f9fe35a5582a42a9973b1ecd0859f"  # Replace with your specific commit hash
-JAX_MD_URL = f"jax-md @ git+https://github.com/jax-md/jax-md.git@{JAX_MD_COMMIT}"
-
-
-# JAX-MD dependencies (from its pyproject.toml, excluding jax/jaxlib) 
-# JAX_MD_DEPS = [
-#     "absl-py",
-#     "numpy",
-#     "flax",
-#     "jraph",
-#     "einops",
-#     "ml_collections",
-#     "e3nn-jax",
-#     "dm-haiku",
-#     "optax",
-#     "frozendict",
-#     "pymatgen",
-# ]
-
-
 setup(
     name="vivarium",
     version="0.2.0",
     license="MIT",
     packages=find_packages(),    
         
-    # Base JAX installation (CPU-only)
     install_requires=[
-        "jax",
-        "jaxlib",
-        # f"jax=={JAX_VERSION}",
-        # f"jaxlib=={JAX_VERSION}",
-        JAX_MD_URL,
-        "protobuf==5.29.5",
+        "jax==0.8.2",
+        "jaxlib==0.8.2",
+        "jax-md==0.2.27",
+        "protobuf==6.31.1",
         "grpcio==1.76.0",
-        "grpcio-tools==1.71.2",
-        "grpcio-health-checking",
-        "panel==1.8.3",
-        "param==2.2.1",
+        "grpcio-tools==1.76.0",
+        "grpcio-health-checking==1.76.0",
+        "panel==1.8.5",
+        "param==2.3.1",
         "hydra-core==1.3.2",
-        "psutil",
-        "pytest"
-        # Add other dependencies here
-    ], # + JAX_MD_DEPS,
+        "psutil==7.2.1",
+        "pytest==9.0.2",
+        "python-dotenv==1.2.1",
+    ],
     
-    # Optional dependencies for CUDA support
     extras_require={
-        "cuda11": [
-            f"jax[cuda11]", #=={JAX_VERSION}",
-        ],
-        "cuda12": [
-            f"jax[cuda12]", #=={JAX_VERSION}",
-        ],
-        "cuda13": [
-            f"jax[cuda13]", #=={JAX_VERSION}",
-        ],
-        "ngrok": [
-            "pyngrok",
-        ],
-        # You can add more variants as needed
+        "cuda11": ["jax[cuda11]"],
+        "cuda12": ["jax[cuda12]"],
+        "cuda13": ["jax[cuda13]"],
+        "ngrok": ["pyngrok"],
     },
     
     author="Clément Moulin-Frier",
