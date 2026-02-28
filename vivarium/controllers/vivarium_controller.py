92cffad3f9ee88d1ecabb1a893f3644ce9cae7f4
Clément Moulin-Frier <clement.moulinfrier@gmail.com>
2026-02-28 22:16:04 +0100

    Add controller routines

--- vivarium/controllers/vivarium_controller.py
+++ vivarium/controllers/vivarium_controller.py
@@ -17,6 +17,7 @@ from vivarium.simulator.grpc_server.simulator_client import SimulatorGRPCClient
 from vivarium.simulator.controller import SimulatorController
 from vivarium.utils.scene_configs import load_scene_config
 from vivarium.utils.timer import sleep_timer
+from vivarium.controllers.utils import Logger, RoutineHandler
 
 
 logging.basicConfig(level=logging.INFO)
@@ -62,6 +63,8 @@ class VivariumController:
         self._interface_process = None
         self._ngrok_active = False
         self.interface_url = None
+        self.logger = Logger()
+        self.routine_handler = RoutineHandler()
 
         # Initialize in disconnected state
         self.client = None
@@ -423,10 +426,35 @@ class VivariumController:
         changes = self.fetch_changes()
         self.client.step(changes)
 
+    def attach_routine(self, routine_fn, name=None, interval=1):
+        """Attach a routine to the controller.
+
+        :param routine_fn: routine function that takes the controller as argument
+        :param name: routine name, defaults to None (routine function name)
+        :param interval: routine execution interval, defaults to 1
+        """
+        self.routine_handler.attach_routine(routine_fn, name, interval)
+
+    def detach_routine(self, routine_fn):
+        """Detach a routine from the controller.
+
+        :param routine_fn: routine function or its name as a string
+        """
+        self.routine_handler.detach_routine(routine_fn)
+
+    def detach_all_routines(self):
+        """Detach all routines from the controller."""
+        self.routine_handler.detach_all_routines()
+        
+    def print_routines(self):
+        """Print the controller routines"""
+        self.routine_handler.print_routines()        
+
     def controller_step(self, catch_errors=True):
         # Step through controllers (e.g. routines and behaviors)
         for _, controller in self.controllers.items():
             controller.step(time=self.time, catch_errors=catch_errors)
+        self.routine_handler.routine_step(self, self.time, catch_errors)
 
     def step(self, catch_errors=True):
         changed_applied = False
